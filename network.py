85353dc043c73335fdb2db920a80eaaa97d2f234
THSK4U <157622490+THSK4U@users.noreply.github.com>
2025-12-31 05:07:51 +0100

    Refactor network communication functions

--- network.py
+++ network.py
@@ -1,10 +1,32 @@
 import socket
 import pickle
+import struct
+
+def send_msg(sock, data):
+    msg = pickle.dumps(data)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+def recv_msg(sock):
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    return pickle.loads(recvall(sock, msglen))
+
+def recvall(sock, n):
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
 
 class Network:
-    def __init__(self):
+    def __init__(self, server_ip="localhost"):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "localhost" # بدل هنا ل ip لي مخدم server 
+        self.server = server_ip
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p_id = self.connect()
@@ -12,13 +34,14 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return pickle.loads(self.client.recv(2048 * 8))
-        except:
+            return recv_msg(self.client)
+        except Exception as e:
+            print(f"Connection failed: {e}")
             pass
 
     def send(self, data):
         try:
-            self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(2048 * 8))
+            send_msg(self.client, data)
+            return recv_msg(self.client)
         except socket.error as e:
             print(e)
